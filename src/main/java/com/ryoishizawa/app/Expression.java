5272d9599179d13c405c336f26e2340549a6f3c9
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2020-11-06 23:37:12 +0800

    upload tdd practice

---
+++ src/main/java/com/ryoishizawa/app/Expression.java
@@ -0,0 +1,7 @@
+package com.ryoishizawa.app;
+
+public interface Expression {
+    Expression times(int multiplier);
+    Expression plus(Expression addend);
+    Money reduce(Bank bank, String to);
+}
---
+++ src/main/java/com/ryoishizawa/app/Money.java
@@ -0,0 +1,46 @@
+package com.ryoishizawa.app;
+
+public class Money implements Expression {
+    protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public Expression times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+
+    public Money reduce(Bank bank, String to) {
+//        return this;
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && currency().equals(money.currency());
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    public String toString() {
+        return amount + " " + currency;
+    }
+}
